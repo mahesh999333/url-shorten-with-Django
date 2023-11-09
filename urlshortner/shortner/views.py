5ab8bc075d895084a8ddd942fd94eb5926be129c
mahesh999333 <105484079+mahesh999333@users.noreply.github.com>
2023-11-09 20:30:13 +0530

    added delete api

--- urlshortner/shortner/views.py
+++ urlshortner/shortner/views.py
@@ -3,18 +3,35 @@ import uuid
 from .models import Url
 from django.http import HttpResponse
 
-# Create your views here.
-def index(request):
-    return render(request, 'index.html')
 
 def create(request):
     if request.method == 'POST':
         link = request.POST['link']
+        link_in_db = Url.objects.filter(link=link)
+        if link_in_db.exists():
+            return HttpResponse(link_in_db[0].uuid)
         uid = str(uuid.uuid4())[:5]
         new_url = Url(link=link,uuid=uid)
         new_url.save()
         return HttpResponse(uid)
 
 def go(request, pk):
-    url_details = Url.objects.get(uuid=pk)
-    return redirect(url_details.link)
+    try:
+        url_details = Url.objects.get(uuid=pk)
+        if url_details:
+            return redirect(url_details.link)
+    except:
+        return HttpResponse("Page not found.")
+    
+def delete(request):
+    try:
+        if request.method == "DELETE":
+            short_url = request.GET['link']
+            uid = short_url.split('/')[-1]
+            url_details = Url.objects.get(uuid=uid)
+            if url_details:
+                Url.objects.filter(uuid=uid).delete()
+                return HttpResponse("Link deleted successfully.")
+            return HttpResponse("Link does not exist.")
+    except:
+        return HttpResponse("Link does not exist.")
